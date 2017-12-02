33c8d85351c60acbf1d2044484a8d941696a977b
vaishnavi.middela <vaishnavi.middela@CPX-XC6POGBU74Q.dir.svc.accenture.com>
2017-12-02 17:53:47 +0100

    Basic project files

---
+++ src/main/java/com/transavia/assignment/App.java
@@ -0,0 +1,14 @@
+package com.transavia.assignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+}
